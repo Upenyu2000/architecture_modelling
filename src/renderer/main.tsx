d974eae9b175976a1311ee9a24db4535e931bdcd
Upenyu <80830651+Upenyu2000@users.noreply.github.com>
2026-07-14 00:27:08 +0100

    Enable double-click free-form vertex insertion

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { RoomEditorDoubleClick } from './components/RoomEditorDoubleClick';
 import './styles.css';
 import './room-editor.css';
 import './production.css';
@@ -8,6 +9,8 @@ import './interior.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RoomEditorDoubleClick>
+      <App />
+    </RoomEditorDoubleClick>
   </React.StrictMode>,
 );
