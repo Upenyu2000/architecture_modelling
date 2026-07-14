13633378b7676f1dde0caa190a674dbc5253dfd2
Upenyu <80830651+Upenyu2000@users.noreply.github.com>
2026-07-14 17:20:48 +0100

    Activate fixed right workspace and scroll routing

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { RoomEditorDoubleClick } from './components/RoomEditorDoubleClick';
+import { WorkspaceScrollGuard } from './components/WorkspaceScrollGuard';
 import './styles.css';
 import './room-editor.css';
 import './production.css';
@@ -10,11 +11,14 @@ import './stability-1.5.2.css';
 import './viewport-1.5.3.css';
 import './runtime-1.5.4.css';
 import './standalone-layout-1.5.5.css';
+import './fixed-workspace-1.5.6.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RoomEditorDoubleClick>
-      <App />
-    </RoomEditorDoubleClick>
+    <WorkspaceScrollGuard>
+      <RoomEditorDoubleClick>
+        <App />
+      </RoomEditorDoubleClick>
+    </WorkspaceScrollGuard>
   </React.StrictMode>,
 );
