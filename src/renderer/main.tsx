1e17bfa012711cab5c0173f47b69c8efc1e2088d
Upenyu <80830651+Upenyu2000@users.noreply.github.com>
2026-07-15 10:08:30 +0100

    load Roomify 2.0 theme

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -11,6 +11,7 @@ import './viewport-1.5.3.css';
 import './runtime-1.5.4.css';
 import './runtime-1.6.1.css';
 import './standalone-layout-1.5.5.css';
+import './roomify-2.0.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
